23122e4b807810acfbd8d3a360806248e64124e6
Hongbing Ye <hongbing@jun18.cs.yorku.ca>
2015-10-08 17:13:31 -0400

    Added readCompany method

--- src/a1.java
+++ src/a1.java
@@ -822,6 +822,36 @@ public class a1 extends JPanel implements ActionListener {
 		return students;
 
 	}
+	
+	// read companies.txt file return string array.
+	private static ArrayList<String> readCompany(){
+		String fileName = "companies.txt";
+		String line = null;
+		ArrayList<String> companies = new ArrayList<String>();
+		try {
+			FileReader fileReader = 
+					new FileReader(fileName);
+			BufferedReader bufferedReader = 
+					new BufferedReader(fileReader);
+
+			while((line = bufferedReader.readLine()) != null) {
+				companies.add(line);
+			}   
+			bufferedReader.close();         
+		}
+		// Exception handling
+		catch(FileNotFoundException ex) {
+			System.out.println(
+					"Unable to open file '" + 
+							fileName + "'");                
+		}
+		catch(IOException ex) {
+			System.out.println(
+					"Error reading file '" 
+							+ fileName + "'");                  
+		}
+		return companies;
+	}
 
 
 	/**
